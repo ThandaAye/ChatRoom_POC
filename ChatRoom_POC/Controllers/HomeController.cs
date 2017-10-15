70c9ccdc24de6bbea4b7c3865957509f6e00e98b
Min Ko Thet <dev.minko@gmail.com>
2017-10-15 23:31:10 +0800

    Clean up

--- ChatRoom_POC/Controllers/HomeController.cs
+++ ChatRoom_POC/Controllers/HomeController.cs
@@ -13,48 +13,47 @@ namespace ChatRoom_POC.Controllers
         public ActionResult Index()
         {
             // create user
-            //ChatRoomContext context = new ChatRoomContext();
-            //User user = new User();
-            //user.Username = "thanda aye";
-            //context.User.Add(user);
+            ChatRoomContext context = new ChatRoomContext();
+            User user = new User();
+            user.Username = "thanda aye";
+            context.User.Add(user);
 
             // create admin
             //Admin admin = new Admin();
             //admin.Username = "Thet Min Ko";
             //admin.Email = "dev.minko@gmail.com";
             //context.User.Add(admin);
-            //context.SaveChanges();
+            context.SaveChanges();
             return View();
         }
 
         public ActionResult About()
         {
             // create chatroom
-            //ChatRoomContext context = new ChatRoomContext();
-            //ChatRoom chatRoom = new ChatRoom();
-            //chatRoom.Owner = context.User.Find(1);
-            //context.ChatRoom.Add(chatRoom);
-            //context.SaveChanges();
-
-            // User user = context.User.Find(1);
-            // ViewBag.Something = user.ChatRoom.Messages;
-            //ChatRoom chatRoom = context.ChatRoom.Where(x => x.Owner == user).FirstOrDefault();
-            //ViewBag.Something = chatRoom.Messages.ToString();
-
-            //ChatRoom chatRoom = context.ChatRoom.Find(1);
-            //ViewBag.Message = chatRoom.Messages;
+            ChatRoomContext context = new ChatRoomContext();
+            ChatRoom chatRoom = new ChatRoom();
+            chatRoom.Owner = context.User.Find(1);
+            context.ChatRoom.Add(chatRoom);
+            context.SaveChanges();
             return View();
         }
 
         public ActionResult Contact()
         {
+            // get user messages
             ChatRoomContext context = new ChatRoomContext();
             User user = context.User.Find(1);
-            Message message = new Message();
-            message.ChatRoom = context.ChatRoom.Find(1);
-            message.CreatedBy = context.User.Find(1);
-            context.Message.Add(message);
-            context.SaveChanges();
+            ChatRoom chatRoom = context.ChatRoom.Where(x => x.OwnerID == user.UserID).FirstOrDefault();
+            ViewBag.Something = chatRoom.Messages.ToString();
+
+            // create message
+            //ChatRoomContext context = new ChatRoomContext();
+            //User user = context.User.Find(1);
+            //Message message = new Message();
+            //message.ChatRoom = context.ChatRoom.Find(1);
+            //message.CreatedBy = context.User.Find(1);
+            //context.Message.Add(message);
+            //context.SaveChanges();
             return View();
         }
 
